2b0e5ab5e97f0fd1e60d0fc50010f8995f346da9
Cayden Wagner <78619267+cwagner2325@users.noreply.github.com>
2023-03-09 04:53:48 +0000

    First implementation of IBankAccount interface.

---
+++ include/IBankAccount.h
@@ -0,0 +1,39 @@
+//********************************************************
+// File name:  IBankAccount.h
+// Author:     Cayden Wagner
+// Date:       3/8/2023
+// Class:      CS380
+// Assignment: Bank
+// Purpose:    To define the header file the IBankAccount 
+//********************************************************
+
+#pragma once
+#include <iostream>
+#include <string>
+
+class IBankAccount 
+{
+  private:
+
+    int mAccountNumber;
+
+    long long mBalance;
+
+    double mInterestRate;
+      
+  public:
+
+    IBankAccount(int, long long, double);
+    virtual ~IBankAccount();
+
+    virtual void deposit(long long);
+    virtual void withdraw(long long);
+
+    virtual void updateMonth() = 0;
+
+    virtual std::string toString() const; 
+
+    virtual bool equals(int);
+
+    friend std::ostream& operator<<(std::ostream&, const IBankAccount&);
+};
---
+++ src/IBankAccount.cpp
@@ -0,0 +1,96 @@
+//********************************************************
+// File name:  IBankAccount.cpp
+// Author:     Cayden Wagner
+// Date:       3/8/2023
+// Class:      CS380
+// Assignment: Bank
+// Purpose:    Implementation for IBankAccount
+//********************************************************
+
+#include "IBankAccount.h"
+#include <string>
+#include <iostream>
+
+//***************************************************************************
+// Constructor:    
+//
+// Description: 
+//
+// Parameters:  
+//***************************************************************************
+IBankAccount::IBankAccount(int accountNumber, long long balance, double interestRate)
+{
+  mAccountNumber = accountNumber;
+  mBalance = balance;
+  mInterestRate = interestRate;
+}
+
+//***************************************************************************
+// Destructor:    
+//
+// Description: 
+//***************************************************************************
+IBankAccount::~IBankAccount() {}
+
+//***************************************************************************
+// Function:    
+//
+// Description: 
+//
+// Parameters:  
+//
+// Returned:    
+//***************************************************************************
+void IBankAccount::deposit(long long depositAmount)
+{
+  mBalance += depositAmount;
+}
+
+//***************************************************************************
+// Function:    
+//
+// Description: 
+//
+// Parameters:  
+//
+// Returned:    
+//***************************************************************************
+void IBankAccount::withdraw(long long withdrawAmount)
+{
+  mBalance -= withdrawAmount;
+}
+//***************************************************************************
+// Function:    
+//
+// Description: 
+//
+// Parameters:  
+//
+// Returned:    
+//***************************************************************************
+std::string IBankAccount::toString() const
+{
+  return mAccountNumber + ", $" + std::to_string(mBalance) + ", " +
+         std::to_string(mInterestRate) + "%, ";
+}
+
+bool IBankAccount::equals(int otherAccount)
+{
+  return mAccountNumber == otherAccount;
+}
+
+//***************************************************************************
+// Function:    
+//
+// Description: 
+//
+// Parameters:  
+//
+// Returned:    
+//***************************************************************************
+std::ostream& operator<<(std::ostream& rcOut, const IBankAccount& rcAccount)
+{
+  rcOut << rcAccount.toString();
+  
+  return rcOut;
+}
