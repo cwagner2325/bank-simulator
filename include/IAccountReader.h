183e83101d15b2ec35f6186f852f90fa76012574
Cayden Wagner <78619267+cwagner2325@users.noreply.github.com>
2023-03-09 06:02:54 +0000

    Implemented IAccountReader interface.

---
+++ include/IAccountReader.h
@@ -0,0 +1,26 @@
+//********************************************************
+// File name:  IAccountReader.h
+// Author:     Cayden Wagner
+// Date:       3/8/2023
+// Class:      CS380
+// Assignment: Bank
+// Purpose:    To define the header file the IAccountReader 
+//********************************************************
+
+#pragma once
+#include <iostream>
+#include <string>
+#include <memory>
+
+#include "IBankAccount.h"
+
+
+class IAccountReader 
+{
+  public:
+
+    virtual ~IAccountReader();
+
+    virtual std::shared_ptr<IBankAccount> readAccount() = 0;
+
+};
---
+++ src/IAccountReader.cpp
@@ -0,0 +1,14 @@
+//********************************************************
+// File name:  IAccountReader.cpp
+// Author:     Cayden Wagner
+// Date:       3/8/2023
+// Class:      CS380
+// Assignment: Bank
+// Purpose:    To implement the IAccountReader 
+//********************************************************
+
+#include <iostream>
+
+#include "IAccountReader.h"
+
+IAccountReader::~IAccountReader() {}
