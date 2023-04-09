901ba2a4dee093f336b1164cbf02c30782f07556
Cayden Wagner <78619267+cwagner2325@users.noreply.github.com>
2023-04-09 19:47:26 +0000

    Implemented currency.

---
+++ include/Currency.h
@@ -0,0 +1,42 @@
+//********************************************************
+// File name:  Currency.h
+// Author:     Cayden Wagner
+// Date:       3/8/2023
+// Class:      CS380
+// Assignment: Bank
+// Purpose:    To define the header file Currency 
+//********************************************************
+
+#pragma once
+
+#include <iostream>
+#include <string>
+
+class Currency
+{
+  public:
+    Currency();
+    Currency(std::string);
+    Currency(const Currency&);
+
+    virtual ~Currency();
+
+    std::string to_string() const;
+    friend std::ostream& operator<<(std::ostream&, const Currency&);
+
+    void from_str(std::string cStr);
+    friend std::istream& operator>>(std::istream&, Currency&);
+
+    friend bool operator==(const Currency&, const Currency&);
+
+    enum class Type 
+    {
+      USD,
+      GBP,
+      EUR,
+      YEN,
+    };
+  
+  private:
+    Currency::Type meType;
+};
---
+++ src/Currency.cpp
@@ -0,0 +1,156 @@
+//********************************************************
+// File name:  Currency.cpp
+// Author:     Cayden Wagner
+// Date:       3/8/2023
+// Class:      CS380
+// Assignment: Bank
+// Purpose:    To implement Currency 
+//********************************************************
+
+#include <iostream>
+#include <string>
+
+#include "Currency.h"
+
+//***************************************************************************
+// Constructor:    Currency
+//
+// Description:    defualt constructor for Currency
+//***************************************************************************
+Currency::Currency() {};
+
+//***************************************************************************
+// Constructor:    Currency
+//
+// Description:    Initalizes a currency from a string
+//
+// Parameters:     cStr - the string that is converted to currency
+//***************************************************************************
+Currency::Currency(std::string cStr)
+{
+  from_str(cStr);
+};
+
+//***************************************************************************
+// Constructor:    Currency
+//
+// Description:    Copy constructor for currency class
+//
+// Parameters:     rcCurr - the currency that is copied to currency
+//***************************************************************************
+Currency::Currency(const Currency& rcCurr)
+{
+  meType = rcCurr.meType;
+};
+
+//***************************************************************************
+// Destructor:    Currency
+//
+// Description:   destructor for Currency
+//***************************************************************************
+Currency::~Currency() {};
+
+//***************************************************************************
+// Function:      to_string
+//
+// Description:   converts a currency type to a string
+//
+// Parameters:    none
+//
+// Returned:      the currency in string from
+//***************************************************************************
+std::string Currency::to_string() const
+{
+  switch(meType)
+  {
+    case Type::USD:
+      return "USD";
+    case Type::GBP:
+      return "GBP";
+    case Type::EUR:
+      return "EUR";
+    case Type::YEN:
+      return "YEN";
+    default:
+      return "";
+  }
+}
+
+//***************************************************************************
+// Operator:    <<
+//
+// Description: prints the currency to a stream
+//
+// Parameters:  rcOut      - the stream that is printed to 
+//              rcCurrency - the currency that is printed
+//
+// Returned:    a reference to the stream variable
+//***************************************************************************
+std::ostream& operator<<(std::ostream& rcOut, const Currency& rcCurrency)
+{
+  return rcOut << rcCurrency.to_string();
+}
+
+//***************************************************************************
+// Operator:    >>
+//
+// Description: reads in a currency from a stream
+//
+// Parameters:  rcIn       - the stream that is read from
+//              rcCurrency - the currency that is read in
+//
+// Returned:    a reference to the stream variable
+//***************************************************************************
+std::istream& operator>>(std::istream& rcIn, Currency& rcCurrency)
+{
+  std::string cTemp;
+  rcIn >> cTemp;
+
+  rcCurrency.from_str(cTemp);
+
+  return rcIn;
+}
+
+//***************************************************************************
+// Operator:    ==
+//
+// Description: compares the equality of two currency's types
+//
+// Parameters:  rcCurr1 - the first currency that is compared
+//              rcCurr2 - the second currency that is compared
+//
+// Returned:    true if the currency types match, else false
+//***************************************************************************
+bool operator==(const Currency& rcCurr1, const Currency& rcCurr2)
+{
+  return rcCurr1.meType == rcCurr2.meType;
+}
+
+//***************************************************************************
+// Operator:    
+//
+// Description: 
+//
+// Parameters:  
+//
+// Returned:    
+//***************************************************************************
+void Currency::from_str(std::string cStr)
+{
+  if (cStr == "USD")
+  {
+    meType = Currency::Type::USD;
+  }
+  else if (cStr == "GMP")
+  {
+    meType = Currency::Type::GBP;
+  }
+  else if (cStr == "EUR")
+  {
+    meType = Currency::Type::EUR;
+  }
+  else if (cStr == "YEN")
+  {
+    meType = Currency::Type::YEN;
+  }
+}
